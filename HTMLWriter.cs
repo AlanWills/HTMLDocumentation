ae3280f3dfe46c9bb9f121bfce63a6a165af1cbb
Alan Wills <alawi@DESKTOP-SVQ7ST9.(none)>
2016-07-16 00:44:17 +0100

    Pushing initial work on auto doc generation

---
+++ HTMLWriter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace HTMLDocumentation
+{
+    public class HTMLWriter : StreamWriter
+    {
+        #region Properties and Fields
+
+        private Type Type { get; set; }
+
+        private string Indentation { get; set; }
+
+        #endregion
+
+        public HTMLWriter(string docsDirectory, Type type) :
+            base(docsDirectory + type.Name + ".html", false)
+        {
+            Type = type;
+        }
+
+        public void WriteType(Type type)
+        {
+            WriteLine("<!DOCTYPE html/>");
+            WriteLine("<html>");
+
+            WriteLine("<head>");
+            Indent();
+                WriteLine("<title>" + type.Name + "</title>");
+            UnIndent();
+            WriteLine("</head>");
+
+            WriteLine("<body>");
+            Indent();
+                WriteLine("<header>");
+                WriteLine("<h1>" + type.Name + "</h1>");
+                WriteLine("</header>");
+
+            foreach (FieldInfo property in type.GetFields())
+            {
+                if (type.Name == property.DeclaringType.Name)
+                {
+                    WriteLine("<h4>" + property.Name + "</h4>");
+                }
+            }
+
+            /*
+             * Have headers + bookmarks for each section (Instance and static, then subsections of public & non-public)
+             * Going to need to write instance & static of public and non public methods declared in this class.
+             * Also indicate if they are overriding virtual functions (if they are only)
+             * Don't write property setters and getters - do that in properties - remove the setters and getters when we iterate over properties?
+             * Parameters, return types, template arguments etc.
+             */
+
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
+            {
+                if (type.Name == method.DeclaringType.Name && 
+                   !method.Name.StartsWith("get_") &&
+                   !method.Name.StartsWith("set_"))
+                {
+                    WriteLine("<h4>" + method.Name + "</h4>");
+                }
+            }
+
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                if (type.Name == method.DeclaringType.Name &&
+                   !method.Name.StartsWith("get_") &&
+                   !method.Name.StartsWith("set_"))
+                {
+                    WriteLine("<h4>" + method.Name + "</h4>");
+                }
+            }
+
+            UnIndent();
+            WriteLine("</body>");
+
+            WriteLine("</html>");
+        }
+
+        /// <summary>
+        /// Overwrite the writeline function with our own implementation that uses the indentation
+        /// </summary>
+        /// <param name="line"></param>
+        private new void WriteLine(string line)
+        {
+            base.WriteLine(Indentation + line);
+        }
+        
+        private void Indent()
+        {
+            Indentation += "\t";
+        }
+
+        private void UnIndent()
+        {
+            Indentation = Indentation.Remove(Indentation.Length - 1);
+        }
+
+        public override void Flush()
+        {
+            base.Flush();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace HTMLDocumentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the relative paths of the '.dll's you wish to document separated by a space");
+
+            string projectsString = Console.ReadLine();
+            string[] projects = projectsString.Split(' ');
+
+            foreach (string projectsPath in projects)
+            {
+                DocumentAssembly(projectsPath);
+            }
+        }
+
+        private static void DocumentAssembly(string projectPath)
+        {
+            string absoluteDirectory = Directory.GetCurrentDirectory() + "\\" + projectPath;
+            Assembly assembly = Assembly.Load("HTMLDocumentation");
+            //Assembly assembly = Assembly.LoadFile(absoluteDirectory);
+
+            string docsDirectory = @"C:\Users\alawi\Documents\\Documentation\\" + assembly.GetName().Name;
+            Directory.CreateDirectory(docsDirectory);
+            docsDirectory += "\\";
+
+            string lastName = "";
+            foreach (Type type in assembly.GetTypes())
+            {
+                // Overwrite the file if it exists
+                using (HTMLWriter writer = new HTMLWriter(docsDirectory, type))
+                {
+                    writer.WriteType(type);
+                    lastName = docsDirectory + type.Name + ".html";
+                }
+            }
+
+            Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", lastName);
+        }
+
+        // Need assembly info but also file structure
+        // Have a skip attribute
+        // Document properties, fields, events
+        // Document methods and their parameters, return types etc. templated arguments?
+        // Only methods for this class or ones that override virtual ones. 
+        // Work on C++ too?
+        // For each directory, add a "link" page as it were to all the directories and files directly in it
+    }
+}
